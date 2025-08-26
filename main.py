b30f90b1aaa9992108dceba29d4833f8461ba702
Codyk1ng <codyking248@gmail.com>
2025-08-25 23:22:22 -0400

    B: capped frame rate 60

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
 
-
+        dt = clock.tick(60)/1000
         pygame.display.flip()#LAST
 
 
